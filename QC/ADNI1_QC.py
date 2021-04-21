b16c52d7ac23518764aaaa9c403e3693c82ecfa3
nxdens <liw82@pitt.edu>
2021-04-21 18:29:37 -0400

    Updated script to handle missing files

--- QC/ADNI1_QC.py
+++ QC/ADNI1_QC.py
@@ -1,4 +1,4 @@
-import json
+# import json
 import sys
 import time
 import os
@@ -6,7 +6,7 @@ import threading
 
 PIDS = []
 DEBUG = False
-
+INVALIDS = []
 #lowercase before checking
 EXIT_STRINGS = ['exit','-1']
 
@@ -28,10 +28,11 @@ def check_settings():
             settings = json.load(f)
     else:
         name = input('Enter your firstname\n')
-        Cerebro = input('Enter your path to Cerebro (Whatever directory Set that lets you access the Studies folder)\n')
+        Cerebro = input('Enter your path to Cerebro (Whatever directory that lets you access the Studies folder)\n')
+        settings = {}
         settings['name'] = name
         settings['Cerebro'] = Cerebro
-        with open('settings.json') as f:
+        with open('settings.json','w') as f:
             json.dump(settings,f)
     return settings['name'], settings['Cerebro']
 
@@ -45,21 +46,34 @@ def open_image(cerebro,subjectID,scanID):
     suffix = "step01_structural_processing/wstrip_mMPRAGE.nii"
     imagepath = toADNI+"ADNI1_Structural_Processing/ADNI1_data/{sub}/{scan}/".format(sub=subjectID,scan=scanID)+suffix
     mask = toADNI+"misc/rmask_ICV.nii"
-    command = "itksnap -g {base}/{imgPath} -s {base}/{maskPath}".format(base=cerebro,imgPath=imagepath,maskPath=mask)
-    pid = os.system(command)
+    exists = os.path.exists("{base}/{imgPath}".format(base=cerebro,imgPath=imagepath))
+    print(exists)
+    command = "itksnap -g {base}/{imgPath} -s {base}/{maskPath} &".format(base=cerebro,imgPath=imagepath,maskPath=mask)
+    if exists:
+        pid = os.system(command)
+    return  exists
 
 def main():
     name = 'test'
     data, order, position,name,cerebro = on_start()
     counter = 0
     while position<len(order):
-        currentKey= list(data.keys())[order[position]]
-        currentScan = data[currentKey]
-        t = threading.Thread(target=open_image, args=(cerebro,currentScan['SubjectID'],currentScan['ScanID'],))
-        t.start()
+        if counter%10 == 0 and counter > 0:
+            print('Please close all open itk-snap windows')
+            print('Take a break!')
+            time.sleep(5*60)
+        
+        valid = False
+        while not valid:
+            currentKey= list(data.keys())[order[position]]
+            currentScan = data[currentKey]
+            valid = open_image(cerebro,currentScan['SubjectID'],currentScan['ScanID'])
+            if not valid:
+                position += 1
         time.sleep(3)
-        #pid = open_image("/home/wangl15@acct.upmchs.net/Documents/Cerebro",currentScan['SubjectID'],currentScan['ScanID'])
+        #pid = open_image(cerebro,currentScan['SubjectID'],currentScan['ScanID'])
         print(position) if DEBUG else None
+        print(pid) if DEBUG else None
         valid = False
         while not valid:
             userInput = input("Enter 1 for a pass or 0 for a fail.\nType 'exit' to exit.\n")
@@ -68,18 +82,21 @@ def main():
                 print(currentKey) if DEBUG else None
                 valid = True
                 position += 1
+                counter += 1
             elif userInput == '0':
                 data[currentKey]['QC'] = 0
                 valid = True
                 position += 1
+                counter += 1
             elif str(userInput).lower() in EXIT_STRINGS:
-                print(position)
-                print(currentKey)
+                print("Last position: {pos}".format(pos = position))
+                print("Last subject: {sub}".format(sub= currentKey))
                 on_close(name,data,position)
                 print('Thanks!')
-                return 
+                return
             else:
                 print('Invalid input')
                 
 if __name__ =='__main__':
     main()
+    
